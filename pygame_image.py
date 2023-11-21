743dc02b954504fd551285cf032f70cd0162da91
c0b22039 <c0b22039da@edu.teu.ac.jp>
2023-11-21 17:09:59 +0900

    演習２

--- pygame_image.py
+++ pygame_image.py
@@ -9,26 +9,18 @@ def main():
     bg_img = pg.image.load("ex01/fig/pg_bg.jpg")#1
     kk_img = pg.image.load("ex01/fig/3.png")#2
     kk_img = pg.transform.flip(kk_img,True,False)#2
-    kk_imgs = [kk_img,pg.transform.rotozoom(kk_img,2,1.0),
-               pg.transform.rotozoom(kk_img,4,1.0),
-               pg.transform.rotozoom(kk_img,6,1.0),
-               pg.transform.rotozoom(kk_img,8,1.0),
-               pg.transform.rotozoom(kk_img,10,1.0),
-               pg.transform.rotozoom(kk_img,8,1.0),
-               pg.transform.rotozoom(kk_img,6,1.0),
-               pg.transform.rotozoom(kk_img,4,1.0),
-               pg.transform.rotozoom(kk_img,2,1.0),]
-    # kk_img1 = [kk_img,pg.transform.rotozoom(kk_img,5,1.0)]
+    bg_img1 = pg.transform.flip(bg_img,True,False)
+    kk_imgs = [pg.transform.rotozoom(kk_img,i,1.0)for i in range(11)]#ex1
+    cnt=[0,1,2,3,4,5,6,7,8,9,10,9,8,7,6,5,4,3,2,1]#ex1
     tmr = 0
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return
-
-        x=tmr%1600#6
+        x=tmr%3200#6
         screen.blit(bg_img, [-x, 0])#4
-        screen.blit(bg_img, [1600-x, 0])#6
-        # screen.blit(kk_img1[tmr%2],[300,200])#5
-        screen.blit(kk_imgs[tmr%10],[300,200])
+        screen.blit(bg_img1, [1600-x, 0])#ex2
+        screen.blit(bg_img, [3200-x, 0])#ex2
+        screen.blit(kk_imgs[cnt[tmr%20]],[300,200])#ex1
         pg.display.update()
         tmr += 1        
         clock.tick(100)
